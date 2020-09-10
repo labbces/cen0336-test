7d14bffddd7b9ada09012ea7a9ecf374fc9fbc14
Diego M Riano <diriano@gmail.com>
2020-09-10 23:13:37 +0000

    Primeiro script do python - imprimir nome, cor, atividade e animal favorito

--- first_script.py
+++ first_script.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
+import sys
+
 name = 'Diego'
-print('My name: ', name)
+favorite_color = 'laranja'
+favorite_activity = 'ler'
+favorite_animal = 'aguia'
+print('Meu nome: ', name)
+print('Minha cor favorita: ', favorite_color)
+print('Minha atividade favorita: ', favorite_activity)
+print('Meu animal favorito: ', favorite_animal)
+
