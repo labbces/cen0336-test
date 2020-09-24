3795ada30cfa33308d74a25b98ed90f6fcc29365
Diego M Riano <diriano@gmail.com>
2020-09-24 03:39:49 +0000

    second_script.py

---
+++ second_script.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import sys
+
+print('Meu nome é:', sys.argv[1], ', meu animal favorito é:', sys.argv[3], ', minha cor favorita é:' , sys.argv[2], 'e minha atividade favorita é:',sys.argv[4])
+print('Meu nome é:', sys.argv[1], ', meu animal favorito é:', sys.argv[3], ', minha cor favorita é:' , sys.argv[2], 'e minha atividade favorita é:',sys.argv[4], sep="CEN0336")
+print('Meu nome é:' +  sys.argv[1] + ', meu animal favorito é:' + sys.argv[3] + ', minha cor favorita é:' + sys.argv[2] + 'e minha atividade favorita é:' + sys.argv[4])
