29ff939bb9be1e1f1d0db6893cf6e9921684e75f
Diego M Riano <diriano@gmail.com>
2020-09-24 03:38:39 +0000

    modifying first_script.py

--- first_script.py
+++ first_script.py
@@ -11,3 +11,10 @@ print('Minha cor favorita: ', favorite_color)
 print('Minha atividade favorita: ', favorite_activity)
 print('Meu animal favorito: ', favorite_animal)
 
+
+
+
+
+
+print('Meu nome é:', sys.argv[1], ', meu animal favorito é:', sys.argv[3], ', minha cor favorita é:' , sys.argv[2], 'e minha atividade favorita é:',sys.argv[4])
+
